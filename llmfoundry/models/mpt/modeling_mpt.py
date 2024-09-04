2018badb56f8129ef515fa2509832c964548c9df
relogu <lollonasi97@gmail.com>
2024-09-04 09:11:40 +0100

    placed todo for substituting the ffn_config

--- llmfoundry/models/mpt/modeling_mpt.py
+++ llmfoundry/models/mpt/modeling_mpt.py
@@ -1387,9 +1387,7 @@ class ComposerMPTCausalLM(HuggingFaceModel):
         additional_train_metrics = additional_train_metrics or []
 
         model = self.model_class(self.config_class(**kwargs))
-        # NOTE: Convert the mutable dictionary to an immutable one to allow for
-        # PyTorch compilation of the forward function
-        model.config.ffn_config = types.MappingProxyType(model.config.ffn_config)
+        # TODO: Substitute in-place the mutable dictionary `model.config.ffn_config` with an immutable object to allow for PyTorch compilation of the forward function. The type used for the substitution must also be pickable to allow for the creation of the checkpoints.
 
         use_train_metrics = use_train_metrics
         train_metric_names = DEFAULT_CAUSAL_LM_TRAIN_METRICS + additional_train_metrics
