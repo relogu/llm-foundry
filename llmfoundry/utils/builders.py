31f1c8a4d531bb61c7a3c7b3f0fdde77f59dd45c
Iacob Alexandru-Andrei <iacobalexandruandrei@gmail.com>
2025-07-03 11:17:07 +0100

    Add protocol which enable a model to define its own param groups

--- llmfoundry/utils/builders.py
+++ llmfoundry/utils/builders.py
@@ -9,7 +9,15 @@ import os
 import re
 import warnings
 from collections import OrderedDict
-from typing import Any, ContextManager, Iterable, Optional, Union
+from typing import (
+    Any,
+    ContextManager,
+    Iterable,
+    Optional,
+    Protocol,
+    Union,
+    runtime_checkable,
+)
 
 import torch
 from composer.core import Algorithm, Callback, Evaluator
@@ -69,7 +77,6 @@ def build_evaluators(
     device_eval_microbatch_size: Optional[Union[int, str, float]] = None,
     destination_dir: Optional[str] = None,
 ) -> tuple[list[Evaluator], list[str], Optional[EvalGauntlet]]:
-
     evaluators = []
     if eval_loader_config is not None:
         evaluators = build_eval_loaders(
@@ -86,15 +93,17 @@ def build_evaluators(
             raise ValueError(
                 'device_eval_batch_size should be an int for icl tasks.',
             )
-        icl_evaluators, logger_keys, eval_gauntlet_callback = build_icl_data_and_gauntlet(
-            icl_tasks_config=icl_tasks_config,
-            eval_gauntlet_config=eval_gauntlet_config,
-            tokenizer=tokenizer,
-            device_eval_batch_size=device_eval_batch_size,
-            icl_seq_len=icl_seq_len,
-            icl_subset_num_batches=icl_subset_num_batches,
-            destination_dir=destination_dir,
-            device_eval_microbatch_size=device_eval_microbatch_size,
+        icl_evaluators, logger_keys, eval_gauntlet_callback = (
+            build_icl_data_and_gauntlet(
+                icl_tasks_config=icl_tasks_config,
+                eval_gauntlet_config=eval_gauntlet_config,
+                tokenizer=tokenizer,
+                device_eval_batch_size=device_eval_batch_size,
+                icl_seq_len=icl_seq_len,
+                icl_subset_num_batches=icl_subset_num_batches,
+                destination_dir=destination_dir,
+                device_eval_microbatch_size=device_eval_microbatch_size,
+            )
         )
         evaluators.extend(icl_evaluators)
 
@@ -116,7 +125,7 @@ def build_eval_loaders(
         is_multi_eval = False
     else:
         raise ValueError(
-            f'Got invalid type for eval_loader_config: {type(eval_loader_config)}, {eval_loader_config=}',
+            f"Got invalid type for eval_loader_config: {type(eval_loader_config)}, {eval_loader_config=}",
         )
 
     for eval_config in eval_configs:
@@ -127,7 +136,7 @@ def build_eval_loaders(
             device_eval_batch_size,
         )
         eval_loader: Evaluator = Evaluator(
-            label=f'eval/{label}' if is_multi_eval else 'eval',
+            label=f"eval/{label}" if is_multi_eval else 'eval',
             dataloader=eval_dataloader,
             # Load the eval data to fail fast. metrics will get added
             # later in add_metrics_to_eval_loaders, after the model is loaded
@@ -186,7 +195,7 @@ def build_icl_data_and_gauntlet(
             eval_gauntlet = eval_gauntlet_config
         else:
             raise ValueError(
-                f'Got invalid type for eval_gauntlet_config: {type(eval_gauntlet_config)}',
+                f"Got invalid type for eval_gauntlet_config: {type(eval_gauntlet_config)}",
             )
         eval_gauntlet['logger_keys'] = logger_keys
         eval_gauntlet['benchmark_sizes'] = {
@@ -280,8 +289,8 @@ def build_composer_model(
     if master_weights_dtype is not None:
         if master_weights_dtype not in str_dtype_to_torch_dtype:
             raise ValueError(
-                f'Invalid master_weights_dtype: {master_weights_dtype}. ' +
-                f'Valid options are: {list(str_dtype_to_torch_dtype.keys())}.',
+                f"Invalid master_weights_dtype: {master_weights_dtype}. " +
+                f"Valid options are: {list(str_dtype_to_torch_dtype.keys())}.",
             )
         dtype = str_dtype_to_torch_dtype[master_weights_dtype]
         model = model.to(dtype=dtype)
@@ -301,7 +310,7 @@ def build_callback(
             kwargs = {}
         if 'train_config' in kwargs:
             raise ValueError(
-                f'`train_config` is a reserved keyword for callbacks with config. Please remove it from the kwargs.',
+                f"`train_config` is a reserved keyword for callbacks with config. Please remove it from the kwargs.",
             )
         kwargs['train_config'] = copy.deepcopy(train_config)
         registry_to_use = registry.callbacks_with_config
@@ -433,14 +442,14 @@ def _extract_param_groups(
             for n, p in model.named_parameters():
                 if re.search(str_match, n):
                     p.requires_grad = False
-                    log.debug(f'Setting `{n}.requires_grad = False`.')
+                    log.debug(f"Setting `{n}.requires_grad = False`.")
 
     param_groups_config = optimizer_config.pop('param_groups', None)
     if param_groups_config is not None:
         params = []
         param_dict = OrderedDict((n, p) for n, p in model.named_parameters())
 
-        log.debug(f'Default optimizer settings: {optimizer_config}.')
+        log.debug(f"Default optimizer settings: {optimizer_config}.")
         for param_group_config in param_groups_config:
             str_match = param_group_config.pop('param_str_match')
             filter_fn = functools.partial(re.search, str_match)
@@ -449,9 +458,11 @@ def _extract_param_groups(
             group_params.update(param_group_config)
 
             log.debug(
-                f'Creating optimizer param_group with parameters: {param_names} ' +\
-                f'(extracted using {str_match=}). The param_group optimizer ' +\
-                f'setting overrides are: {param_group_config}.')
+                f"Creating optimizer param_group with parameters: {param_names} "
+                +
+                f"(extracted using {str_match=}). The param_group optimizer " +
+                f"setting overrides are: {param_group_config}.",
+            )
 
             params.append(group_params)
 
@@ -461,14 +472,68 @@ def _extract_param_groups(
     return model.parameters()
 
 
+@runtime_checkable
+class HasGetParamGroups(Protocol):
+    """Protocol for models that can provide their optimizer parameter groups.
+
+    This is useful for models that have custom parameter groups or need to
+    modify the optimizer configuration based on their parameters.
+    Such as models using MuP or CompleteP.
+    """
+
+    # NOTE: This is a protocol, so it can be used with isinstance checks.
+    # NOTE: The method signature must match the one in the model.
+
+    def get_optimizer_param_groups(  # <- exact signature
+        self,
+        optimizer_config: dict[str, Any],
+    ) -> tuple[Iterable[torch.Tensor] | Iterable[dict[str, Any]], dict[str, Any]]:
+        """Get the optimizer parameter groups and any modified optimizer config.
+
+        Parameters:
+        -----------
+            optimizer_config (dict[str, Any]): The optimizer configuration.
+
+        Returns:
+        --------
+            tuple[Iterable[torch.Tensor] | Iterable[dict[str, Any]], dict[str,
+            Any]]: A tuple containing the parameter groups and any modified
+            optimizer configuration.
+        """
+        ...
+
+
 def build_optimizer(
     model: torch.nn.Module,
     name: str,
     optimizer_config: dict[str, Any],
 ) -> Optimizer:
+    # NOTE: Modified from Composer's build_optimizer to support models that
+    # have custom parameter groups or need to modify the optimizer configuration
+    # based on their parameters. Such as models using MuP or CompleteP.
+    if isinstance(model, HasGetParamGroups):
+        # NOTE: call to make sure disable_grad is correctly applied
+        # as well as any other transformations
+        # NOTE: incompatible with manually specifying
+        # param groups in optimizer_config
+        tmp_param_groups = _extract_param_groups(model, optimizer_config)
+        if tmp_param_groups != model.parameters():
+            raise ValueError(
+                'The model has custom parameter groups, but the optimizer config '
+                +
+                'does not support this. Please remove the `param_groups` key from '
+                + 'the optimizer config.',
+            )
 
-    params = _extract_param_groups(model, optimizer_config)
-    kwargs = {**optimizer_config}
+        params, new_optim_config = model.get_optimizer_param_groups(
+            optimizer_config,
+        )
+        kwargs = {
+            **new_optim_config,
+        }
+    else:
+        params = _extract_param_groups(model, optimizer_config)
+        kwargs = {**optimizer_config}
 
     if 'params' in kwargs:
         raise ValueError(
@@ -510,8 +575,9 @@ def build_tokenizer(
 
     # NOTE: We add to the end of the file the (world) local rank to support situations
     # where the different nodes share the file system
-    signal_file_path = dist.get_node_signal_file_name(
-    ) + f'_wr_{dist.get_node_rank()!s}'
+    signal_file_path = (
+        dist.get_node_signal_file_name() + f"_wr_{dist.get_node_rank()!s}"
+    )
 
     if dist.is_available() and dist.is_initialized(
     ) and dist.get_world_size() > 1:
@@ -544,7 +610,7 @@ def build_tokenizer(
 
     if not hasattr(tokenizer, 'eos_token') or tokenizer.eos_token is None:
         raise ValueError(
-            f'The tokenizer {tokenizer_name} must have an eos_token.',
+            f"The tokenizer {tokenizer_name} must have an eos_token.",
         )
 
     if dist.is_available() and dist.is_initialized(
@@ -578,7 +644,7 @@ def build_icl_evaluators(
 
     icl_tasks_list = None
     if isinstance(icl_tasks, str):
-        log.info(f'Extracting ICL task config from path: {icl_tasks}')
+        log.info(f"Extracting ICL task config from path: {icl_tasks}")
         with open(icl_tasks, 'r') as icl_f:
             icl_task_cfg = om.load(icl_f)
         icl_tasks_list = to_list_container(icl_task_cfg.icl_tasks)
@@ -609,7 +675,7 @@ def build_icl_evaluators(
             else:
                 icl_task_type = icl_cfg['icl_task_type']
                 raise ValueError(
-                    f'No metric_names defined, unable to build default metrics for icl_task_type={icl_task_type}.',
+                    f"No metric_names defined, unable to build default metrics for icl_task_type={icl_task_type}.",
                 )
 
         if 'max_seq_len' not in icl_cfg:
@@ -619,14 +685,15 @@ def build_icl_evaluators(
 
         if 'num_beams' in icl_cfg:
             raise ValueError(
-                'num_beams is no longer supported as a top level icl_task parameter.'  + \
-                'Please use generation_kwargs.num_beams instead.')
+                'num_beams is no longer supported as a top level icl_task parameter.'
+                + 'Please use generation_kwargs.num_beams instead.',
+            )
 
     for icl_cfg in icl_tasks_list:
         assert isinstance(
             icl_cfg,
             dict,
-        ), f'Expected dict, got {type(icl_cfg)}, {icl_cfg=}'
+        ), f"Expected dict, got {type(icl_cfg)}, {icl_cfg=}"
         _validate_cfg(icl_cfg)
         for num_fewshot in list(icl_cfg['num_fewshot']):
             if tokenizer.pad_token_id is None:
@@ -636,10 +703,10 @@ def build_icl_evaluators(
                 pad_tok_id = tokenizer.pad_token_id
 
             icl_cfg_label = icl_cfg['label']
-            label = f'{icl_cfg_label}/{num_fewshot}-shot'
+            label = f"{icl_cfg_label}/{num_fewshot}-shot"
             metric_names = list(icl_cfg['metric_names'])
             # TODO: fix Composer bug when copying local paths and destination exists
-            destination_path = f'{destination_dir}/{icl_cfg_label}-{num_fewshot}.jsonl'
+            destination_path = f"{destination_dir}/{icl_cfg_label}-{num_fewshot}.jsonl"
             # NOTE: From a preliminary investigation, it seems that MosaicML developers have not
             # been sufficiently smart to handle the potential overwrite when it comes to download
             # one of these datasets from a remote path. As such, they needed to wipe out and
@@ -697,22 +764,24 @@ def build_icl_evaluators(
                 destination_path=destination_path,
                 kwargs=icl_constructor_kwargs,
             )
-            if 'has_categories' in icl_cfg and icl_cfg[
-                'has_categories'] and isinstance(dataloaders, dict):
+            if (
+                'has_categories' in icl_cfg and icl_cfg['has_categories'] and
+                isinstance(dataloaders, dict)
+            ):
                 for category in dataloaders.keys():
                     logger_keys.extend([
-                        f'metrics/{label}/{category}/{m}' for m in metric_names
-                    ])
+                        f"metrics/{label}/{category}/{m}" for m in metric_names
+                    ],)
                     evaluators.append(
                         Evaluator(
-                            label=f'{label}/{category}',
+                            label=f"{label}/{category}",
                             dataloader=dataloaders[category],
                             metric_names=metric_names,
                         ),
                     )
             else:
                 logger_keys.extend([
-                    f'metrics/{label}/{m}' for m in metric_names
+                    f"metrics/{label}/{m}" for m in metric_names
                 ])
                 evaluators.append(
                     Evaluator(
@@ -732,7 +801,6 @@ def build_tp_strategies(
     name: str,
     model: ComposerModel,
 ) -> dict[str, ParallelStyle]:
-
     warnings.warn(
         'Checkpointing is not currently supported for tensor parallelism due to this pytorch bug: https://github.com/pytorch/pytorch/issues/134095#issuecomment-2345018244',
     )
